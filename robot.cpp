26984885f234573998601ac880f4e063c7f426c7
webberandr <66193534+webberandr@users.noreply.github.com>
2020-06-08 13:31:35 +1200

    Add files via upload
    Initial skeleton code

---
+++ robot.cpp
@@ -0,0 +1,18 @@
+#include "robot.hpp"
+
+
+int main(){
+	if (initClientRobot() !=0){
+		std::cout<<" Error initializing robot"<<std::endl;
+	}
+    double vLeft = 3.0;
+    double vRight = 3.0;
+    //SavePPMFile("i0.ppm",cameraView);
+    while(1){
+      takePicture();
+      setMotors(vLeft,vRight);   
+      std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+       usleep(10000);
+  } //while
+
+} // main
