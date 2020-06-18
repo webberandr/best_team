c3651806ca2d9a83e2a13eaf8a2fce6cdb8c0a35
webberandr <66193534+webberandr@users.noreply.github.com>
2020-06-18 16:51:06 +1200

    Add files via upload
    Code that runs completion and core, has a few functions for challenge that have not been implemented.

--- robot.cpp
+++ robot.cpp
@@ -1,5 +1,19 @@
 #include "robot.hpp"
 
+
+/**function for determining if pixels are red*/
+int getRedPixels(int row, int column){
+	int r = get_pixel(cameraView, row, column, 0);
+	int g = get_pixel(cameraView, row, column, 1);
+	int b = get_pixel(cameraView, row, column, 2);
+	int all = r+g+b;
+	float redness = r/all;
+	if (redness<1){
+		return 0;
+	}
+	return redness;
+}
+
 /**function for deciding if a pixel is white or not
  * returns 1 if white, and 0 if not white
  */
@@ -14,19 +28,6 @@ int getWhitePixels(int row, int column){
 	}
 	return white;
 }
-
-int getRedPixels(int row, int column){
-	int red;
-	int pix=get_pixel(cameraView, row, column, 3);
-	if (pix>=250){
-		red=1;
-	}
-	else {
-		red=0;
-	}
-	return red;
-}
-
 /**function that finds difference between centre of line and centre of image
  * positive if right of line, negative if left of line
  * returns that value multiplied by kp.error
@@ -50,83 +51,91 @@ double findError(){
 	return error;
 }
 else {
-	return 0;//returns 0 if no white pixels in row being checked
+	return -1;//returns -1 if no white pixels in row being checked
 }
 	}
+	
 
-/**function that detects if line is in left or right column
- * call with 0 for letf column, call with 149 (or image.width-1) for right column
- * returns true if it finds any white pixels
- * ignores top quarter of image
+/**function that returns true if every pixel in a given row is red
  */
-bool left_or_right(int column){
-	for (int row = 25; row<100; row++){//starts at row 25
-		if (getWhitePixels(row, column)==1){
-			return true;
+bool allRed(int row){
+	for (int column = 0; column<150; column++){
+		if (getRedPixels(row, column)==0){
+			return false;
 		}
 	}
-	return false;
+	return true;
+}
+
+/**function that returns the diffeerence between where the red line is and where we want it to be */
+int redError(int column){
+	for (int row = 25; row<100; row++){
+		//std::cout<<getRedPixels(row, column);
+		if (getRedPixels(row, column)==0){
+			return row;
+		}
+	}
+	std::cout<<std::endl;
+	return -1;
 }
 
 /**function for returning how far down is line from top when on side*/
 double sideError(int column){
+	int min = 25;
 	for (int row = 0; row<100; row++){
 		//std::cout<<getWhitePixels(row, column);
 		if (getWhitePixels(row, column)==1){
+			if (row<min){
+				return min;
+			}
+			else{
 			return row;
 		}
+		}
 	}
-	return 0;
+	return -1;
 }
 
+
 int main(){
-	double turnSpeed=20.0;
-	double forward=40.0;
-	bool sideLine = false;
-	bool rightTurn = true;
+	bool sideLine = false;//changes to true when line detected on sides closer to robot, changes back to false when no line is detected
+	bool rightTurn = true;//sets to false when a left turn is picked up so it doesn't turn back to the previous line being followed when that ends
+	//up on the right of the image, sets back to true once no line detected on left
 	double sideKP = 0.2;
 	double frontKP = 0.07;
-	double turn = 50;
-	int left = 40;
-	int right = 110;
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
-	}
-	//double turn = 
+	} 
     double vLeft;
     double vRight;
-    //SavePPMFile("i0.ppm",cameraView);
     while(1){
-		if (sideError(left)>=turn||sideError(right)>=turn){
+		if (sideError(0)>=70.0||sideError(149)>=70.0){
 			sideLine=true;
 		}
-		if (sideError(left)<=1&&sideError(right)<=1){
+		if (sideError(0)==-1&&(sideError(149)==-1||!rightTurn)){
 			sideLine=false;
 		}
       takePicture();
-      if (!sideLine){
-	  vRight=forward-findError()*frontKP;
-      vLeft=forward+findError()*frontKP;
+      if (!sideLine&&findError()!=-1){
+	  vRight=40.0-findError()*frontKP;
+      vLeft=40.0+findError()*frontKP;
   }
    else {
-	   if (sideError(left)==0){
+	   if (sideError(0)==-1){
 		   rightTurn=true;
 	   }
-	   if (sideError(left)>0&&sideError(right)==0){
+	   if (sideError(0)>0&&sideError(149)==-1){
 		   rightTurn=false;
-		   vRight=turnSpeed+sideError(left)*sideKP;
-		   vLeft=turnSpeed-sideError(left)*sideKP;
+		   vRight=20.0+sideError(0)*sideKP;
+		   vLeft=20.0-sideError(0)*sideKP;
 	   }
-	   else if (sideError(right)>0&&rightTurn){
-		   vRight=turnSpeed-sideError(left)*sideKP;
-		  vLeft=turnSpeed+sideError(right)*sideKP;
+	   else if (sideError(149)>0&&rightTurn){
+		   vRight=20.0-sideError(149)*sideKP;
+		  vLeft=20.0+sideError(149)*sideKP;
 	   }
   }
-      //checkWhitePixels();
       setMotors(vLeft,vRight);
-     // std::cout<<"Error = "<<findError()<<" VPError= "<<vpError()<<std::endl;
       std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-      std::cout<<"RIght Error="<<sideError(right)<<std::endl;
        usleep(10000);
   } //while
 
